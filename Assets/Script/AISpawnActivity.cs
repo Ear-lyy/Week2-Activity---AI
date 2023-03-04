ce9d32ab9d5770ba551729767b33f1b3a0baa5ae
Ear-lyy <earl.crisostomo@ciit.edu.ph>
2023-03-04 15:07:20 +0800

    Destroy Implementation

--- Assets/Script/AISpawnActivity.cs
+++ Assets/Script/AISpawnActivity.cs
@@ -5,42 +5,63 @@ using UnityEngine.AI;
 
 public class AISpawnActivity : MonoBehaviour
 {
-
+    // Start is called before the first frame update
     public NavMeshAgent agent;
     public Transform[] patrolPoints;
-    public int DestinationPoints = 2;
+    public int DestinationPoints = 0;
     public float remDistance = 0.5f;
-    private int pointsVisited = 0;
-    private float agentSpeed = 10.0f;
+    public Transform Player;
+    public static int score;
+    public float radius;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.autoBraking = false;
-        agent.speed = agentSpeed;
+
         GotoNextpoint();
+
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (!agent.pathPending && agent.remainingDistance < remDistance && pointsVisited < patrolPoints.Length)
+
+        if (!agent.pathPending && agent.remainingDistance < remDistance)
         {
             GotoNextpoint();
         }
+
+
+
     }
 
     public void GotoNextpoint()
     {
-        if (patrolPoints.Length == 0 || pointsVisited >= patrolPoints.Length)
+        if (patrolPoints.Length == 0)
         {
+            return;
+        }
+
+        if (DestinationPoints == patrolPoints.Length)
+        {
+
+            agent.destination = transform.position;
             agent.isStopped = true;
+
+            score += 1;
+
+            Debug.Log(score);
+            Destroy(gameObject);
+
             return;
         }
 
         agent.destination = patrolPoints[DestinationPoints].position;
-        DestinationPoints = (DestinationPoints + 1) % patrolPoints.Length;
-        pointsVisited++;
+        DestinationPoints = DestinationPoints + 1;
+
+
     }
+
+
 }
-
-
---
+++ Assets/Script/Invisible.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invisible : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
